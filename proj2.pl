0f38d16da85528ed4b366da687fc19b2eea301a5
xstast24 <stastny.F@seznam.cz>
2018-04-27 19:21:41 +0200

    Rotations & reverse rotations complete

--- proj2.pl
+++ proj2.pl
@@ -212,12 +212,12 @@ rotL_left(
 			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
 		],
 		[
-			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
-			[A1, A2, A3, A4, A5, A6, A7, A8, A9],
+			[C9, E2, E3, C6, E5, E6, C3, E8, E9],
+			[E1, A2, A3, E4, A5, A6, E7, A8, A9],
 			Right,
-			[C1, C2, C3, C4, C5, C6, C7, C8, C9],
+			[C1, C2, F7, C4, C5, F4, C7, C8, F1],
 			[D7, D4, D1, D8, D5, D2, D9, D6, D3],
-			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+			[A1, F2, F3, A4, F5, F6, A7, F8, F9]
 		]).
 
 % rotate left anti-clockwise (looking on this side)
@@ -231,12 +231,12 @@ rotL_left_rev(
 			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
 		],
 		[
-			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
-			[A1, A2, A3, A4, A5, A6, A7, A8, A9],
+			[A1, E2, E3, A4, E5, E6, A7, E8, E9],
+			[F1, A2, A3, F4, A5, A6, F7, A8, A9],
 			Right,
-			[C1, C2, C3, C4, C5, C6, C7, C8, C9],
-			[D7, D4, D1, D8, D5, D2, D9, D6, D3],
-			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+			[C1, C2, E7, C4, C5, E4, C7, C8, E1],
+			[D3, D6, D9, D2, D5, D8, D1, D4, D7],
+			[C9, F2, F3, C6, F5, F6, C3, F8, F9]
 		]).
 
 % rotate back clockwise (looking on this side)
@@ -250,12 +250,12 @@ rotB_back(
 			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
 		],
 		[
-			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
+			[B3, B6, B9, E4, E5, E6, E7, E8, E9],
 			Front,
-			[B1, B2, B3, B4, B5, B6, B7, B8, B9],
+			[B1, B2, F9, B4, B5, F8, B7, B8, F7],
 			[C7, C4, C1, C8, C5, C2, C9, C6, C3],
-			[D1, D2, D3, D4, D5, D6, D7, D8, D9],
-			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+			[E3, D2, D3, E2, D5, D6, E1, D8, D9],
+			[F1, F2, F3, F4, F5, F6, D1, D4, D7]
 		]).
 
 % rotate back anti-clockwise (looking on this side)
@@ -269,12 +269,12 @@ rotB_back_rev(
 			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
 		],
 		[
-			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
+			[D7, D4, D1, E4, E5, E6, E7, E8, E9],
 			Front,
-			[B1, B2, B3, B4, B5, B6, B7, B8, B9],
-			[C7, C4, C1, C8, C5, C2, C9, C6, C3],
-			[D1, D2, D3, D4, D5, D6, D7, D8, D9],
-			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+			[B1, B2, E1, B4, B5, E2, B7, B8, E3],
+			[C3, C6, C9, C2, C5, C8, C1, C4, C7],
+			[F7, D2, D3, F8, D5, D6, F9, D8, D9],
+			[F1, F2, F3, F4, F5, F6, B9, B6, B3]
 		]).
 	
 /******
