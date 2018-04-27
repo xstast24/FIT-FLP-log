bdc4c0bfd5c51c2684d163d37574907edf24b7e4
xstast24 <stastny.F@seznam.cz>
2018-04-28 00:14:09 +0200

    Solving up rotations

--- proj2.pl
+++ proj2.pl
@@ -1,6 +1,6 @@
-/************
- * LOAD INPUT
- ************/
+/********************
+ * LOAD INPUT TO CUBE
+ ********************/
 %Reads line from stdin, terminates on LF or EOF.
 read_line(L,C) :-
 	get_char(C),
@@ -276,7 +276,57 @@ rotB_back_rev(
 			[F7, D2, D3, F8, D5, D6, F9, D8, D9],
 			[F1, F2, F3, F4, F5, F6, B9, B6, B3]
 		]).
+
+/**************
+ * SOLVING CUBE		
+ **************/
+% For given Rubik's cube return list of rubik cube states that lead to the solution.
+solve_rubik(
+		[	
+			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
+			[A1, A2, A3, A4, A5, A6, A7, A8, A9],
+			[B1, B2, B3, B4, B5, B6, B7, B8, B9],
+			[C1, C2, C3, C4, C5, C6, C7, C8, C9],
+			[D1, D2, D3, D4, D5, D6, D7, D8, D9],
+			[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+		],
+		SolutionSteps) :- 
+				get_solution_steps(
+					[		
+						[E1, E2, E3, E4, E5, E6, E7, E8, E9],
+						[A1, A2, A3, A4, A5, A6, A7, A8, A9],
+						[B1, B2, B3, B4, B5, B6, B7, B8, B9],
+						[C1, C2, C3, C4, C5, C6, C7, C8, C9],
+						[D1, D2, D3, D4, D5, D6, D7, D8, D9],
+						[F1, F2, F3, F4, F5, F6, F7, F8, F9]
+					],
+					[	
+						[E5, E5, E5, E5, E5, E5, E5, E5, E5],
+						[A5, A5, A5, A5, A5, A5, A5, A5, A5],
+						[B5, B5, B5, B5, B5, B5, B5, B5, B5],
+						[C5, C5, C5, C5, C5, C5, C5, C5, C5],
+						[D5, D5, D5, D5, D5, D5, D5, D5, D5],
+						[F5, F5, F5, F5, F5, F5, F5, F5, F5]
+					],
+					[],
+					SolutionSteps,
+					2, % TODO make this as a parameter in main
+					_).
+
+% found solution, set ReachedDepth to false, so it is accepted
+get_solution_steps(Rubik, Rubik, SolutionSteps, SolutionSteps, _, ReachedDepth) :- write("Nasel"),ReachedDepth is 0, !.
+% iterations reached max depth and no solution found
+get_solution_steps(_, _, _, _, 0, ReachedDepth) :- write("Fail"), ReachedDepth is 1, !.
+% TODO		
+get_solution_steps(Rubik, RubikDesired, PreviousSteps, SolutionSteps, MaxDepth, ReachedDepth) :-
+	rotU_up(Rubik, TmpRubik),
+	append(PreviousSteps, [TmpRubik], TmpSteps),
+	TmpDepth is MaxDepth - 1,
+	get_solution_steps(TmpRubik, RubikDesired, TmpSteps, SolutionSteps, TmpDepth, ReachedDepth),
 	
+	ReachedDepth == 0.
+
+ 
 /******
  * MAIN
  ******/
@@ -284,12 +334,19 @@ start :-
 	prompt(_, ''),
 	read_input(Input),
 	input_to_rubik(Input, Rubik),
-	write_rubik(Rubik),
+	write_rubik(Rubik), %TODO remove debug print
+	
+	solve_rubik(Rubik, SolutionSteps),
+	
+	write_rubiks(SolutionSteps),
 	halt.
 
 /**************
  * PRINT OUTPUT
  **************/
+write_rubiks([]).
+write_rubiks([Cube | Cubes]) :- write_rubik(Cube), write_rubiks(Cubes).
+
 write_rubik(
 		[
 			[E1, E2, E3, E4, E5, E6, E7, E8, E9],
@@ -304,76 +361,3 @@ write_rubik(
 	writef("%w%w%w %w%w%w %w%w%w %w%w%w\n", [A4, A5, A6, B4, B5, B6, C4, C5, C6, D4, D5, D6]),
 	writef("%w%w%w %w%w%w %w%w%w %w%w%w\n", [A7, A8, A9, B7, B8, B9, C7, C8, C9, D7, D8, D9]),
 	writef("%w%w%w\n%w%w%w\n%w%w%w\n", [F1, F2, F3, F4, F5, F6, F7, F8, F9]).
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/* nacte zadany pocet radku */
-read_lines2([],0).
-read_lines2(Ls,N) :-
-	N > 0,
-	read_line(L,_),
-	N1 is N-1,
-	read_lines2(LLs, N1),
-	Ls = [L|LLs].
-
-/* vypise seznam radku (kazdy radek samostatne) */
-write_lines2([]).
-write_lines2([H|T]) :- writeln(H), write_lines2(T). %(writeln je "knihovni funkce")
-
-/* rozdeli radek na podseznamy -- pracuje od konce radku */
-%zalozit prvni (tzn. posledni) seznam:
-split_line2([],[[]]) :- !.
-%pridat novy seznam:
-split_line2([' '|T], [[]|S1]) :- !, split_line2(T,S1).
-%pridat novy seznam, uchovat oddelujici znak:
-split_line2([H|T], [[],[H]|S1]) :- (H=','; H=')'; H='('), !, split_line2(T,S1).
-%pridat znak do existujiciho seznamu:
-split_line2([H|T], [[H|G]|S1]) :- split_line2(T,[G|S1]).
-
-/* pro vsechny radky vstupu udela split_line2 */
-% vstupem je seznam radku (kazdy radek je seznam znaku)
-split_lines2([],[]).
-split_lines2([L|Ls],[H|T]) :- split_lines2(Ls,T), split_line2(L,H).
-
-/* nacte N radku vstupu, zpracuje, vypise */
-start2(N) :-
-		prompt(_, ''),
-		read_lines2(LL, N),
-		split_lines2(LL,S),
-		write_lines2(S).
-
-
-% prevede retezec na seznam atomu
-% pr.: string("12.35",S). S = ['1', '2', '.', '3', '5'].
-retezec([],[]).
-retezec([H|T],[C|CT]) :- atom_codes(C,[H]), retezec(T,CT).
-
-% prevede seznam cislic na cislo
-% pr.: cislo([1,2,'.',3,5],X). X = 12.35
-cislo(N,X) :- cislo(N,0,X).
-cislo([],F,F).
-cislo(['.'|T],F,X) :- !, cislo(T,F,X,10).
-cislo([H|T],F,X) :- FT is 10*F+H, cislo(T,FT,X).
-cislo([],F,F,_).
-cislo([H|T],F,X,P) :- FT is F+H/P, PT is P*10, cislo(T,FT,X,PT).
-
-% existuje knihovni predikat number_chars(?Number, ?CharList)
-% pr.: number_chars(12.35, ['1', '2', '.', '3', '5']).
